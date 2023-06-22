06baad686068e2b2b234da5cedb08bcdf77fdb72
maall002 <massa.allaimouni002@stud.fh-dortmund.de>
2023-06-22 22:43:04 +0200

    D

--- SGS/src/main/java/com/renewableenergy/SGS/EnergyProducerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.renewableenergy.SGS;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.renewableenergy.SGS.entity.EnergyProducer;
-
-public interface EnergyProducerRepository extends JpaRepository<EnergyProducer,Long> {
-
-}
--- SGS/src/main/java/com/renewableenergy/SGS/exception/TurbineNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.renewableenergy.SGS.exception;
-
-public class TurbineNotFoundException extends RuntimeException {
-	public TurbineNotFoundException(String m) {
-		super(m);
-	}
-
-}
