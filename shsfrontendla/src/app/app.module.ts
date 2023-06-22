0dea6bcdcfc819fec42b4e3fb807d9b39159115b
George <george.al3id@gmail.com>
2023-06-22 21:39:01 +0200

    homepage/addDevices

--- shsfrontendla/src/app/app.module.ts
+++ shsfrontendla/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AddDevicesComponent } from './add-devices/add-devices.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    AddDevicesComponent
   ],
   imports: [
     BrowserModule,
