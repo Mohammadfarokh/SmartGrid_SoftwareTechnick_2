a173ea714f24cf004215270d3912044b39fda32a
George <george.al3id@gmail.com>
2023-06-26 16:40:47 +0200

    homepage

--- shsfrontendla/src/app/app-routing.module.ts
+++ shsfrontendla/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AddDevicesComponent } from './add-devices/add-devices.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'shsfrontendla/src/app/homepage', component: HomepageComponent },
+  { path: 'SGS/src/app/add-Devices', component: AddDevicesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- shsfrontendla/src/app/homepage/homepage.component.ts
+++ shsfrontendla/src/app/homepage/homepage.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+  value1: string = '';
+  value2: string = '';
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchValuesFromBackend();
+  }
+
+  fetchValuesFromBackend() {
+    this.http.get<any>('Backend-link').subscribe(
+      (response) => {
+        this.value1 = response.value1;
+        this.value2 = response.value2;
+      },
+      (error) => {
+        console.log('Error occurred while fetching values from the backend:', error);
+      }
+    );
+  }
 }
