d10aea12a3ccf80ab34acf06117fba30f7d4059e
Amir1998de <amirhossein.vatani001@stud.fh-dortmund.de>
2023-06-26 11:19:51 +0200

    26.06

--- SGS/src/main/java/com/renewableenergy/SGS/SolarpanelRepository.java
+++ SGS/src/main/java/com/renewableenergy/SGS/SolarpanelRepository.java
@@ -1,5 +1,11 @@
 package com.renewableenergy.SGS;
 
-public interface SolarpanelRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.renewableenergy.SGS.entity.Solarpanel;
+
+public interface SolarpanelRepository extends JpaRepository<Solarpanel,Long> {
+
+	void deleteSolarpanelById(Long id);
+	Solarpanel findSolarpanelById(Long id);
 }
