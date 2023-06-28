db39fee32242f64cf303d36dbb61f6c3c8bd367b
George <george.al3id@gmail.com>
2023-06-28 14:12:08 +0200

    Homepage dynamic tables

--- shsfrontendla/src/app/homepage/homepage.component.ts
+++ shsfrontendla/src/app/homepage/homepage.component.ts
@@ -2,6 +2,26 @@ import { HttpClient } from '@angular/common/http';
 
 import { Component, OnInit } from '@angular/core';
 
+interface Battery {
+  name :string,
+  capacity:number
+}
+interface StandardConsumer {
+  name :string,
+  consumed:number
+}
+interface VariableConsumer {
+  name :string,
+  consumed:number
+}
+interface SolarPanel {
+  name :string,
+  maxOutput:number
+}
+interface Turbine {
+  name :string,
+  maxOutput:number
+}
 
 @Component({
   selector: 'app-homepage',
@@ -11,14 +31,75 @@ import { Component, OnInit } from '@angular/core';
 export class HomepageComponent implements OnInit {
   value1: string = '';
   value2: string = '';
+  Batterys :Battery[]=[];
+  StandardConsumers :StandardConsumer[]=[];
+  VariableConsumers :VariableConsumer[]=[];
+  SolarPanels :SolarPanel[]=[];
+  Turbines :Turbine[]=[];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.fetchValuesFromBackend();
+    this.ConsumptionProductionValue();
+    this.getAddedBatterys();
+    this.getAddedSolarPanels();
+    this.getAddedStandardConsumers();
+    this.getAddedTurbines();
+    this.getAddedVariableConsumers();
+
+  }
+  getAddedBatterys() {
+    this.http.get<Battery[]>('your-backend-url/smart-homes').subscribe({
+      next: (response: Battery[]) => {
+        this.Batterys = response;
+      },
+      error: (error: any) => {
+        console.log('Error fetching Batterys:', error);
+      },
+    });
+  }
+  getAddedStandardConsumers() {
+    this.http.get<StandardConsumer[]>('your-backend-url/smart-homes').subscribe({
+      next: (response: StandardConsumer[]) => {
+        this.StandardConsumers = response;
+      },
+      error: (error: any) => {
+        console.log('Error fetching Standard Consumers:', error);
+      },
+    });
+  }
+  getAddedVariableConsumers() {
+    this.http.get<VariableConsumer[]>('your-backend-url/smart-homes').subscribe({
+      next: (response: VariableConsumer[]) => {
+        this.VariableConsumers = response;
+      },
+      error: (error: any) => {
+        console.log('Error fetching Variable Consumers:', error);
+      },
+    });
+  }
+  getAddedSolarPanels() {
+    this.http.get<SolarPanel[]>('your-backend-url/smart-homes').subscribe({
+      next: (response: SolarPanel[]) => {
+        this.SolarPanels = response;
+      },
+      error: (error: any) => {
+        console.log('Error fetching Solar Panels:', error);
+      },
+    });
+  }
+  getAddedTurbines() {
+    this.http.get<Turbine[]>('your-backend-url/smart-homes').subscribe({
+      next: (response: Turbine[]) => {
+        this.Turbines = response;
+      },
+      error: (error: any) => {
+        console.log('Error fetching Turbines:', error);
+      },
+    });
   }
 
-  fetchValuesFromBackend() {
+  ConsumptionProductionValue() {
     this.http.get<any>('Backend-link').subscribe(
       (response) => {
         this.value1 = response.value1;
@@ -29,4 +110,6 @@ export class HomepageComponent implements OnInit {
       }
     );
   }
+
+
 }
