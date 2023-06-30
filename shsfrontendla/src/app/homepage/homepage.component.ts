b137fe1ddb11b4e55aacd12fc2c21422f8c07200
George <george.al3id@gmail.com>
2023-06-30 11:27:27 +0200

    Toggle buttons and update status

--- shsfrontendla/src/app/homepage/homepage.component.ts
+++ shsfrontendla/src/app/homepage/homepage.component.ts
@@ -3,41 +3,41 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 interface Battery {
-  name :string,
-  capacity:number
+  name: string;
+  capacity: number;
 }
 interface StandardConsumer {
-  name :string,
-  consumed:number,
-  artDerTarif:string
+  name: string;
+  consumed: number;
+  artDerTarif: string;
 }
 interface VariableConsumer {
-  name :string,
-  consumed:number
-  artDerTarif:string
+  name: string;
+  consumed: number;
+  artDerTarif: string;
 }
 interface SolarPanel {
-  name :string,
-  maxOutput:number
+  name: string;
+  maxOutput: number;
 }
 interface Turbine {
-  name :string,
-  maxOutput:number
+  name: string;
+  maxOutput: number;
 }
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
 })
 export class HomepageComponent implements OnInit {
-  value1:String ='';
-  value2:String ='';
-  Batteries :Battery[]=[];
-  StandardConsumers :StandardConsumer[]=[];
-  VariableConsumers :VariableConsumer[]=[];
-  SolarPanels :SolarPanel[]=[];
-  Turbines :Turbine[]=[];
+  value1: String = '';
+  value2: String = '';
+  Batteries: Battery[] = [];
+  StandardConsumers: StandardConsumer[] = [];
+  VariableConsumers: VariableConsumer[] = [];
+  SolarPanels: SolarPanel[] = [];
+  Turbines: Turbine[] = [];
 
   constructor(private http: HttpClient) {}
 
@@ -48,7 +48,6 @@ export class HomepageComponent implements OnInit {
     this.getAddedTurbines();
     this.getAddedStandardConsumers();
     this.getAddedVariableConsumers();
-
   }
   getAddedBatterys() {
     this.http.get<Battery[]>('your-backend-url/smart-homes').subscribe({
@@ -61,24 +60,28 @@ export class HomepageComponent implements OnInit {
     });
   }
   getAddedStandardConsumers() {
-    this.http.get<StandardConsumer[]>('your-backend-url/smart-homes').subscribe({
-      next: (response: StandardConsumer[]) => {
-        this.StandardConsumers = response;
-      },
-      error: (error: any) => {
-        console.log('Error fetching Standard Consumers:', error);
-      },
-    });
+    this.http
+      .get<StandardConsumer[]>('your-backend-url/smart-homes')
+      .subscribe({
+        next: (response: StandardConsumer[]) => {
+          this.StandardConsumers = response;
+        },
+        error: (error: any) => {
+          console.log('Error fetching Standard Consumers:', error);
+        },
+      });
   }
   getAddedVariableConsumers() {
-    this.http.get<VariableConsumer[]>('your-backend-url/smart-homes').subscribe({
-      next: (response: VariableConsumer[]) => {
-        this.VariableConsumers = response;
-      },
-      error: (error: any) => {
-        console.log('Error fetching Variable Consumers:', error);
-      },
-    });
+    this.http
+      .get<VariableConsumer[]>('your-backend-url/smart-homes')
+      .subscribe({
+        next: (response: VariableConsumer[]) => {
+          this.VariableConsumers = response;
+        },
+        error: (error: any) => {
+          console.log('Error fetching Variable Consumers:', error);
+        },
+      });
   }
   getAddedSolarPanels() {
     this.http.get<SolarPanel[]>('your-backend-url/smart-homes').subscribe({
@@ -108,10 +111,94 @@ export class HomepageComponent implements OnInit {
         this.value2 = response.value2;
       },
       (error) => {
-        console.log('Error occurred while fetching values from the backend:', error);
+        console.log(
+          'Error occurred while fetching values from the backend:',
+          error
+        );
       }
     );
   }
+  batteryToggleButton(battery: any) {
+    const button = document.getElementById('batteryButton');
+    if (button?.innerHTML === 'OFF') {
+      button.innerHTML = 'ON';
+      this.http
+        .put<any>('http://localhost:9595/solarpanel/update', battery)
+        .subscribe({
+          next: (response) => {
+            console.log('Solarpanel updated successfully:', response);
+          },
+          error: (error) => {
+            console.error('Error updating solarpanel:', error);
+          },
+        });
+    } else button?.innerHTML === 'OFF';
+  }
 
+  StandardConsumerToggleButton(StandardConsumer: any) {
+    const button = document.getElementById('StandardConsumerButton');
+    if (button?.innerHTML === 'OFF') {
+      button.innerHTML = 'ON';
+      this.http
+        .put<any>('http://localhost:9595/solarpanel/update', StandardConsumer)
+        .subscribe({
+          next: (response) => {
+            console.log('Solarpanel updated successfully:', response);
+          },
+          error: (error) => {
+            console.error('Error updating solarpanel:', error);
+          },
+        });
+    } else button?.innerHTML === 'OFF';
+  }
+  VariableConsumerToggleButton(VariableConsumer: any) {
+    const button = document.getElementById('VariableConsumerButton');
+    if (button?.innerHTML === 'OFF') {
+      button.innerHTML = 'ON';
+      this.http
+        .put<any>('http://localhost:9595/solarpanel/update', VariableConsumer)
+        .subscribe({
+          next: (response) => {
+            console.log('Solarpanel updated successfully:', response);
+          },
+          error: (error) => {
+            console.error('Error updating solarpanel:', error);
+          },
+        });
+    } else button?.innerHTML === 'OFF';
+  }
 
+  SolarPanelToggleButton(SolarPanel: any) {
+    const button = document.getElementById('SolarPanelButton');
+    if (button?.innerHTML === 'OFF') {
+      button.innerHTML = 'ON';
+      this.http
+        .put<any>('http://localhost:9595/solarpanel/update', SolarPanel)
+        .subscribe({
+          next: (response) => {
+            console.log('Solarpanel updated successfully:', response);
+          },
+          error: (error) => {
+            console.error('Error updating solarpanel:', error);
+          },
+        });
+    } else button?.innerHTML === 'OFF';
+  }
+
+  TurbineToggleButton(Turbine: any) {
+    const button = document.getElementById('SolarPanelButton');
+    if (button?.innerHTML === 'OFF') {
+      button.innerHTML = 'ON';
+      this.http
+        .put<any>('http://localhost:9595/solarpanel/update', Turbine)
+        .subscribe({
+          next: (response) => {
+            console.log('Solarpanel updated successfully:', response);
+          },
+          error: (error) => {
+            console.error('Error updating solarpanel:', error);
+          },
+        });
+    } else button?.innerHTML === 'OFF';
+  }
 }
